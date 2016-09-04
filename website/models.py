33f63eca886bfcc596aadf46333b2a085edf447d
Luke Hinds <lukehinds@gmail.com>
2016-09-04 12:23:25 +0100

    Initial Commit

---
+++ website/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    ossn = models.CharField(max_length=10)
+    title = models.CharField(max_length=200)
+    releases = models.CharField(max_length=200)
+    discussion = models.TextField()
+    summary = models.TextField()
+    actions = models.TextField()
+    contact = models.CharField(max_length=50)
+    references = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
